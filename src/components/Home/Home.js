9fc8f9d78a4bf2649aa02ca03838c09ddfd5fd89
shafrinahamed97 <shafrinahamedjob@gmail.com>
2023-05-26 11:05:45 +0600

    Remove from cart and not allow duplicate

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import TShirt from '../TShirt/TShirt';
 import Cart from '../Cart/Cart';
@@ -6,6 +6,25 @@ import './Home.css';
 
 const Home = () => {
     const tshirts = useLoaderData();
+    const [cart, setCart] = useState([]);
+
+    const handleAddToCart = tshirt => {
+        const exists = cart.find(ts => ts._id === tshirt._id);
+        if (exists) {
+            alert('t-shirt already added')
+        }
+        else {
+            const newCart = [...cart, tshirt];
+            setCart(newCart);
+            // alert('successfully added')
+        }
+
+    }
+
+    const handleRemoveItem = tshirt => {
+        const remaining = cart.filter(ts => ts._id !== tshirt._id);
+        setCart(remaining);
+    }
     return (
         <div className='home-container'>
             <div className="t-shirt-container">
@@ -13,11 +32,15 @@ const Home = () => {
                     tshirts.map(tshirt => <TShirt
                         key={tshirt._id}
                         tshirt={tshirt}
+                        handleAddToCart={handleAddToCart}
                     ></TShirt>)
                 }
             </div>
             <div className="cart-container">
-                <Cart></Cart>
+                <Cart
+                    cart={cart}
+                    handleRemoveItem={handleRemoveItem}
+                ></Cart>
             </div>
         </div>
     );
